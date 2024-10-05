bbd94b6eef0aa810bedbdd2077e1cd1904386c3c
LEADER <godstark82@gmail.com>
2024-10-05 18:46:08 +0530

    added vercel url

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SERVER_NAME'] = 'subdomain-test-phi.vercel.app'
 
-@app.route('/', subdomain='a')
+@app.route('/', subdomain='<subdomain>')
 def subdomain_route(subdomain):
-    # Your function code here
-    return 'Hello ' + subdomain
+    return f'Hello from subdomain: {subdomain}'
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'subdomain': None})
+@app.route('/', subdomain='<subdomain>')
+def index(subdomain):
+    if subdomain:
+        return f'Hello from subdomain: {subdomain}'
     return render_template('index.html')
-
 

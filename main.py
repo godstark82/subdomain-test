255c0fdd7137b0f30b7aefb5f3e7e5cf8ef7fa63
LEADER <godstark82@gmail.com>
2024-10-05 18:23:05 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/<subdomain>', subdomain='<subdomain>')
+def index(subdomain):
+    return 'Hello, World! ' + subdomain
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
